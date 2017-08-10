0b4ba89842591c8b390e52b9591853b0a1d0667c
Toni Suominen <toni.suominen@suominen.net>
2017-08-10 16:32:09 +0300

    Update CameraSizeToVelocity2D.cs

--- CameraSizeToVelocity2D.cs
+++ CameraSizeToVelocity2D.cs
@@ -2,11 +2,10 @@ using UnityEngine;
 
 public class CameraSizeToVelocity2D : MonoBehaviour
 {
-	public Rigidbody2D target;
-
-	[SerializeField]
+    public Rigidbody2D target;
+    [SerializeField]
     private float minCam;
-	[SerializeField]
+    [SerializeField]
     private float maxCam;
     private float playerVel;
     private float smooth = 0.5f;
@@ -14,21 +13,21 @@ public class CameraSizeToVelocity2D : MonoBehaviour
 
     void Start()
     {
-		cam = Camera.main;
+    	cam = Camera.main;
     }
 
     void Update()
     {
-        if (target != null)
-            {
-				//clamp the size of the camera to desired min and max values, use rigidbodys velocity vectors X value for horizontal movement, Y value for vertical movement
-				float cameraSizeInput = Mathf.Clamp(Mathf.Abs(target.velocity.x), minCam, maxCam);
+    	if (target != null)
+        {
+		//clamp the size of the camera to desired min and max values, use rigidbodys velocity vectors X value for horizontal movement, Y value for vertical movement
+		float cameraSizeInput = Mathf.Clamp(Mathf.Abs(target.velocity.x), minCam, maxCam);
 				
-				//use SmoothDamp to smoothly transition between the values
+		//use SmoothDamp to smoothly transition between the values
                 float cameraSizeChanger = Mathf.SmoothDamp(cam.orthographicSize, cameraSizeInput, ref playerVel, smooth);
 
-				//assign the value to the camera
-                cam.orthographicSize = cameraSizeChanger;
-            }
+		//assign the value to the camera
+               	cam.orthographicSize = cameraSizeChanger;
+        }
      }
 }
